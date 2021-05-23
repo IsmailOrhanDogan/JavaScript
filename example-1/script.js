153fdb3bc8baa1f6ebc57f442dc34e65ee47359a
Ismail Orhan Dogan <ismailorhandogan@gmail.com>
2021-05-23 21:21:42 +0200

    1st day

---
+++ example-1/script.js
@@ -0,0 +1,23 @@
+console.log("Hello JavaScript");
+console.log("JavaScript easier than Java Language.");
+console.error("This is an error message");
+console.warn("This is a warning message");
+// alert("Hello!");
+
+//   ** ==================== DEGISKEN TANIMLAMA ================== ** // 
+
+var pi = 3.14;
+console.log(pi);
+console.log("Type of pi: " + typeof pi);
+
+pi = "3";
+console.log(pi);
+console.log("Type of pi: " + typeof pi);
+
+pi = true;
+console.log(pi);
+console.log("Type of pi: " + typeof pi);
+
+isim = "Ismail";
+console.log(isim);
+console.log("Type of isim: " + typeof isim);
