0238d154e3f2266cb5eae7399cbb020526c74bdb
Ismail Orhan Dogan <ismailorhandogan@gmail.com>
2021-05-30 20:37:58 +0200

    9th day

---
+++ stage4_dom/example3_domManipule/script.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".on").onclick = function () {
+    document.querySelector(".image").src = "./img/lamba_on.gif";
+  };
+  
+  document.querySelector(".off").onclick = function () {
+    document.querySelector(".image").src = "./img/lamba_off.gif";
+  };
